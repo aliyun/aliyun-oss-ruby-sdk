713b92e9eb92f76618537b140e254476007fec18
Tianlong Wu <tianlong.wtl@alibaba-inc.com>
2015-11-11 11:51:26 +0800

    add resumable download example

---
+++ examples/resumable_download.rb
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+
+require 'yaml'
+require_relative '../lib/oss'
+
+# 初始化OSS Bucket
+Aliyun::OSS::Logging.set_log_level(Logger::DEBUG)
+cred_file = "~/.oss.yml"
+cred = YAML.load(File.read(File.expand_path(cred_file)))
+oss = Aliyun::OSS::Client.new(
+  'oss.aliyuncs.com', cred["id"], cred["key"]).get_bucket('t-hello-world')
+
+oss.resumable_download('resumable', '/tmp/x')
