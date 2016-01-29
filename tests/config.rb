ffa80bc1b5b1dce5bcfb2250d0931014b5efb553
Tianlong Wu <tianlong.wtl@alibaba-inc.com>
2016-01-29 12:27:05 +0800

    add the missing tests/config.rb

---
+++ tests/config.rb
@@ -0,0 +1,15 @@
+class Config
+  class << self
+    def creds
+      {
+        access_key_id: ENV['RUBY_SDK_OSS_ID'],
+        access_key_secret: ENV['RUBY_SDK_OSS_KEY'],
+        endpoint: ENV['RUBY_SDK_OSS_ENDPOINT']
+      }
+    end
+
+    def bucket
+      ENV['RUBY_SDK_OSS_BUCKET']
+    end
+  end
+end
